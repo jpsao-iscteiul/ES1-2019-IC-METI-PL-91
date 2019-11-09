5b0d451c541462fbbf5dccd2d29abc861f809665
dmsfa-iscteiul <dmsfa@iscte-iul.pt>
2019-11-09 20:16:20 +0000

    4th commit

--- ESProject/src/main/java/ES1_2019_IC_METI_PL_91/ESProject/gui.java
+++ ESProject/src/main/java/ES1_2019_IC_METI_PL_91/ESProject/gui.java
@@ -1,16 +1,11 @@
 package ES1_2019_IC_METI_PL_91.ESProject;
 
-import javax.swing.JFrame;
+public class gui{
+	
 
-public class gui extends JFrame{
-	
-	
-	public void gui() {
-		JFrame gui = new JFrame("GUI");
-	}
 
 	public static void main(String [] args) {
-		System.out.println("3th");
+		System.out.println("4th");
 	}
 
 }
