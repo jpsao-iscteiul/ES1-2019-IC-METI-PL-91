43d51b753cf1fa7dd13b403adf2d1d70d2607af7
dmsfa-iscteiul <dmsfa@iscte-iul.pt>
2019-11-09 20:30:12 +0000

    commit first interface

--- ESProject/src/main/java/ES1_2019_IC_METI_PL_91/ESProject/gui.java
+++ ESProject/src/main/java/ES1_2019_IC_METI_PL_91/ESProject/gui.java
@@ -1,11 +1,24 @@
 package ES1_2019_IC_METI_PL_91.ESProject;
 
-public class gui{
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class gui extends JFrame{
 	
-
-
+	public gui() {
+	JFrame frame = new JFrame("GUI"); 
+	frame.setSize(200, 200);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	JLabel rule = new JLabel("RULE");
+	frame.add(rule);
+	frame.setVisible(true);
+	
+	}
+	
 	public static void main(String [] args) {
 		System.out.println("4th");
+		new gui();
 	}
 
 }
