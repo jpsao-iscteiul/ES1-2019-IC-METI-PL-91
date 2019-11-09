5062ad82ad808705226a97df15cfe554cb67d009
Diogo <Diogo@Diogo>
2019-11-09 19:29:13 +0000

    First gui commit to shared repo.

---
+++ ESProject/src/main/java/ES1_2019_IC_METI_PL_91/ESProject/gui.java
@@ -0,0 +1,17 @@
+package ES1_2019_IC_METI_PL_91.ESProject;
+
+import javax.swing.JFrame;
+
+public class gui extends JFrame{
+	
+	
+	public void gui() {
+		JFrame gui = new JFrame("GUI");
+	}
+
+	public static void main(String [] args) {
+		System.out.println("-.-");
+	}
+
+}
+
